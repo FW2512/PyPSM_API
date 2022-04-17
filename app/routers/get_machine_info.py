6b2edcc43ff249634047c0ffdb3b980e41d45e91
Firas jarmakani <firasjarmakani@gmail.com>
2022-04-17 16:16:05 +0300

    Add new route /getstatus

--- app/routers/get_machine_info.py
+++ app/routers/get_machine_info.py
@@ -17,3 +17,15 @@ def getinfo(current_user = Depends(oauth2.get_current_user)):
 
     else:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+@router.get("/getstatus", response_model=schemas.MachineStatus)
+def get_status(current_user = Depends(oauth2.get_current_user)):
+    database.cursor.execute("SELECT user_id FROM users WHERE user_id = %s", [current_user.id])
+    user_info = database.cursor.fetchone()
+    if user_info:
+        database.cursor.execute("SELECT * FROM users LEFT JOIN machine_status ON machine_status.user_id = users.user_id WHERE users.user_id = %s;", [current_user.id])
+        machine_status = database.cursor.fetchone()
+        return machine_status
+
+    else:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
