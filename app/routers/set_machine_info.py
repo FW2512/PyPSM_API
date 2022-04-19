f55cfd74a26bbbb0148f5485764817ada2f6e05e
Firas jarmakani <firasjarmakani@gmail.com>
2022-04-20 01:15:50 +0300

    add comma to the query

--- app/routers/set_machine_info.py
+++ app/routers/set_machine_info.py
@@ -22,7 +22,7 @@ def set_status(machine_status: schemas.MachineStatus, current_user=Depends(oauth
     database.cursor.execute("SELECT user_id FROM users WHERE user_id = %s", [current_user.id])
     recived_info = database.cursor.fetchone()
     if recived_info:
-        database.cursor.execute("""UPDATE machine_status SET online = %s shutdown = %s WHERE user_id = %s RETURNING *""",
+        database.cursor.execute("""UPDATE machine_status SET online = %s, shutdown = %s WHERE user_id = %s RETURNING *""",
                                 [machine_status.online, machine_status.shutdown, current_user.id])
         sent_data = database.cursor.fetchone()
         database.conn.commit()
