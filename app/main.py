5e766cb1cef9fc9790a091cf4e99c864e994a6fa
Firas jarmakani <firasjarmakani@gmail.com>
2022-04-29 02:42:42 +0300

    Add set and get screenshots routers

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-from .routers import users, set_machine_info, get_machine_info
+from .routers import users, set_machine_info, get_machine_info, get_machine_status, set_machine_status, get_screenshots, set_screenshots
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(set_machine_info.router)
 app.include_router(get_machine_info.router)
+app.include_router(get_machine_status.router)
+app.include_router(set_machine_status.router)
+app.include_router(get_screenshots.router)
+app.include_router(set_screenshots.router)
 
 
 @app.get("/")
