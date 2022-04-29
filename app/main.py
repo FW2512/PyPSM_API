5f03e1909c9eb2fa7f9fe9c83339cf3ebda32595
Firas jarmakani <firasjarmakani@gmail.com>
2022-04-29 03:09:02 +0300

    Add contact info

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from .routers import users, set_machine_info, get_machine_info, get_machine_status, set_machine_status, get_screenshots, set_screenshots
 
-app = FastAPI()
+app = FastAPI(
+    title="PyPSM",
+    version="3.0.0",
+    description="this API created for PyPSM which a software for monitor and manage your PC remotely from PyPSM android app",
+    contact={"GitHub": "https://github.com/FW2512",
+             "Telegram": "https://t.me/FirasJarmakani",
+             "Gmail": "firasjarmakani@gmail.com"}
+)
 app.include_router(users.router)
 app.include_router(set_machine_info.router)
 app.include_router(get_machine_info.router)
@@ -13,7 +20,14 @@ app.include_router(set_screenshots.router)
 
 @app.get("/")
 def root():
-    return {"message": "welcome to PyPSM API!", 
-            "description": "this API created for PyPSM which a software for monitor and manage your PC remotely from PyPSM android app", 
-            "developed_by": "FirasJarmakani",
-            "contact_info":"more details will be available soon"}
+    return {
+        "message": "welcome to PyPSM API!", 
+        "description": "this API created for PyPSM which a software for monitor and manage your PC remotely from PyPSM android app", 
+        "developed_by": "FirasJarmakani",
+        "contact_info":
+            {
+                "GitHub": "https://github.com/FW2512",
+                "Telegram": "https://t.me/FirasJarmakani",
+                "Gmail": "firasjarmakani@gmail.com"
+            }
+            }
